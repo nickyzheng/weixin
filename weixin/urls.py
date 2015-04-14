b805e9b2e797e6531342acd896231c6a914a2bc8
nickyzheng <nicky.zheng@avnet.com>
2015-04-14 14:23:06 +0800

    m

--- weixin/urls.py
+++ weixin/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'wxapp.views.home'),
+    url(r'^wx/', 'wxapp.views.home'),
 ]
