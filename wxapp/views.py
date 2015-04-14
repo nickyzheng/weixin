cc64a426b1cfe88964577b57a52539a8df704ea9
nickyzheng <nicky.zheng@avnet.com>
2015-04-14 21:42:03 +0800

    commit by shell script

--- wxapp/views.py
+++ wxapp/views.py
@@ -7,37 +7,46 @@ import datetime
 #from django.template.loader import get_template
 #from django.template import Context
 from django.shortcuts import render_to_response
+import traceback
 
 ####### END of TEST ########
 
 # Create your views here.
 def home(req):
     print '---> in home'
-    print req.get_host()
-    print req.get_full_path()
-    print req.body
+    print 'host: ', req.get_host()
+    print 'path: ', req.get_full_path()
+    print 'body: ', req.body
+    print 'method: ', req.method
     print '---> after print'
-    if req.method == 'GET':
-        if 'signature' in req.GET:
-            signature = req.GET['signature']
-            echostr = req.GET['echostr']
-            timestamp = req.GET['timestamp']
-            nonce = req.GET['nonce']
+    try:
+        if req.method == 'GET':
+            if 'signature' in req.GET:
+                signature = req.GET['signature']
+                echostr = req.GET['echostr']
+                timestamp = req.GET['timestamp']
+                nonce = req.GET['nonce']
 
-            token = '1stloop'
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
+                token = '1stloop'
+                list = [token, timestamp, nonce]
+                list.sort()
+                sha1 = hashlib.sha1()
+                map(sha1.update, list)
+                hashcode = sha1.hexdigest()
 
-            if hashcode == signature:
-                return HttpResponse(echostr) 
-        return HttpResponse('end of get') 
-    if req.method == 'POST':
-        print '---> in POST'
-        print req
-        return HttpResponse('post') 
+                if hashcode == signature:
+                    return HttpResponse(echostr) 
+            return HttpResponse('end of get') 
+        if req.method == 'POST':
+            print '---> in POST'
+            print req
+            return HttpResponse('post') 
+        
+    except:
+        tb = traceback.format_exc()
+        print '---> in tb'
+        print tb
+        return HttpResponse(tb)
 
 
 def test(req):
