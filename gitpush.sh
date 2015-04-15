3aaa7d6cf139128d3e1bc310ab9610f5fa4348f3
nickyzheng <nicky.zheng@avnet.com>
2015-04-15 14:17:54 +0800

    commit by shell script

--- gitpush.sh
+++ gitpush.sh
@@ -3,7 +3,5 @@ git add *
 git commit -m "commit by shell script"
 git push origin master
 ssh james@1stloop.com
-expect james@1stserver
 cd ~/codes/sandbox/weixin
-git pull origin master
-exit
+./gitpull.sh
