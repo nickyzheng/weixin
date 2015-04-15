551848f36f2a540a28146383935d35815911c469
nickyzheng <nicky.zheng@avnet.com>
2015-04-15 13:33:04 +0800

    commit by shell script

--- wxapp/views.py
+++ wxapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import hashlib
+import lxml
+import time
+import os
+from lxml import etree
 
 ####### TEST ###############
 import datetime
@@ -20,27 +24,32 @@ def home(req):
     print 'method: ', req.method
     print '---> after print'
 
-    if req.method == 'GET':
-        if 'signature' in req.GET:
-            signature = req.GET['signature']
-            echostr = req.GET['echostr']
-            timestamp = req.GET['timestamp']
-            nonce = req.GET['nonce']
+    if req.method == 'GET' and 'signature' in req.GET:
+        signature = req.GET['signature']
+        echostr = req.GET['echostr']
+        timestamp = req.GET['timestamp']
+        nonce = req.GET['nonce']
 
-            token = '1stloop'
-            list = [token, timestamp, nonce]
-            list.sort()
-            sha1 = hashlib.sha1()
-            map(sha1.update, list)
-            hashcode = sha1.hexdigest()
+        token = '1stloop'
+        list = [token, timestamp, nonce]
+        list.sort()
+        sha1 = hashlib.sha1()
+        map(sha1.update, list)
+        hashcode = sha1.hexdigest()
 
-            if hashcode == signature:
-                return HttpResponse(echostr) 
+        if hashcode == signature:
+            return HttpResponse(echostr) 
         return HttpResponse('end of get') 
     if req.method == 'POST':
         print '---> in POST'
-
-        return HttpResponse('post') 
+        str_xml = req.body
+        xml = etree.fromstring(str_xml)
+        content = xml.find("Content").text
+        msgType = xml.find("MsgType").text
+        fromUser = xml.find("FromUserName").text
+        toUser = xml.find("ToUserName").text
+        print content, msgType, fromUser, toUser
+        return render_to_response('wx_reply_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': content})
         
 
 
