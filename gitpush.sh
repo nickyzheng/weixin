5b8768de656b40a05589dbf2cec287fa424ff989
nickyzheng <nicky.zheng@avnet.com>
2015-04-15 14:07:04 +0800

    commit by shell script

--- gitpush.sh
+++ gitpush.sh
@@ -1,3 +1,8 @@
+cd /Users/jizheng/Documents/Work/Programming/Django/djcode/weixin
 git add *
 git commit -m "commit by shell script"
 git push origin master
+ssh james@1stloop.com
+cd ~/codes/sandbox/weixin
+git pull origin master
+exit
