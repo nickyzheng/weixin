b79a2c7585b0003d060e86024a21031f50c601e6
nickyzheng <nicky.zheng@avnet.com>
2015-04-20 18:14:14 +0800

    commit by shell script

--- wxapp/views.py
+++ wxapp/views.py
@@ -77,15 +77,6 @@ def home(req):
                 command = content.split()
                 if command[0] == 'show':
                     c = clothes.objects.get(name = command[1])
-                    # if c.category and not c.category.isspace():   
-                    #     category = clothes.CATEGORY_LIST[int(c.category)][1]
-                    # else:
-                    #     category = 'not set'
-                    # if c.season and not c.season.isspace():
-                    #     season = clothes.SEASON_LIST[int(c.season)][1]
-                    # else:
-                    #     season = 'not set'
-                    # reply_content = image_text_reply_content % (c.name, category, season, c.tag, str(c.choose_count))
                     reply_content = set_image_text_reply_content(c)
                     picUrl = image_url_prefix + c.image_filename
                     return render_to_response('wx_reply_image_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content, 'picUrl': picUrl})
@@ -103,15 +94,6 @@ def home(req):
                 c = clothes.objects.get(name = command[1])
                 c.name = command[2]
                 c.save()
-                # if c.category and not c.category.isspace():   
-                #     category = clothes.CATEGORY_LIST[int(c.category)][1]
-                # else:
-                #     category = 'not set'
-                # if c.season and not c.season.isspace():
-                #     season = clothes.SEASON_LIST[int(c.season)][1]
-                # else:
-                #     season = 'not set'
-                # reply_content = image_text_reply_content % (c.name, category, season, c.tag, str(c.choose_count))
                 reply_content = set_image_text_reply_content(c)
                 picUrl = image_url_prefix + c.image_filename
                 return render_to_response('wx_reply_image_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content, 'picUrl': picUrl})
@@ -129,15 +111,6 @@ def home(req):
                     c.season = command[3]
                     c.save()
 
-                # if c.category and not c.category.isspace():   
-                #     category = clothes.CATEGORY_LIST[int(c.category)][1]
-                # else:
-                #     category = 'not set'
-                # if c.season and not c.season.isspace():
-                #     season = clothes.SEASON_LIST[int(c.season)][1]
-                # else:
-                #     season = 'not set'
-                # reply_content = image_text_reply_content % (c.name, category, season, c.tag, str(c.choose_count))
                 reply_content = set_image_text_reply_content(c)
                 picUrl = image_url_prefix + c.image_filename
                 return render_to_response('wx_reply_image_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content, 'picUrl': picUrl})
@@ -146,8 +119,9 @@ def home(req):
             p = re.compile(pattern_today)
             if p.match(content):
                 c = clothes.objects.order_by('?')[0]
-
-
+                reply_content = set_image_text_reply_content(c)
+                picUrl = image_url_prefix + c.image_filename
+            return render_to_response('wx_reply_image_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content, 'picUrl': picUrl})
 
         if msgType == 'image':
             PicUrl = xml.find("PicUrl").text
