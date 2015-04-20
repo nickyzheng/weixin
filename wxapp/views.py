4a49cdb5db5c212bfb56628c5f809ee55edbdb79
nickyzheng <nicky.zheng@avnet.com>
2015-04-20 21:13:02 +0800

    commit by shell script

--- wxapp/views.py
+++ wxapp/views.py
@@ -143,8 +143,8 @@ def home(req):
                 reply_content = set_image_text_reply_content(c)
                 picUrl = image_url_prefix + c.image_filename
                 return render_to_response('wx_reply_image_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content, 'picUrl': picUrl})
-            print '---> end of command'
-        else:
+                
+            # nothing match, display menu        
             reply_content = u'命令错误。\nrename 给衣服起个名字\nshow: 显示衣服\nshowall 列出全部衣服\nset cat 设置类型\nset sea 设置季节\ntoday 今天穿什么\ndel 删除\nchoose 今天选这件'
             return render_to_response('wx_reply_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content})
 
