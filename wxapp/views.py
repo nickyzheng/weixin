3db81cbfd78d84b4afb52060a1a7066cecae521a
nickyzheng <nicky.zheng@avnet.com>
2015-04-21 16:20:10 +0800

    commit by shell script

--- wxapp/views.py
+++ wxapp/views.py
@@ -86,6 +86,12 @@ def home(req):
                     for c in all_clothes:
                         reply_content += c.name + '\n'
                     return render_to_response('wx_reply_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content})
+                if command[0] == 'showcat':
+                    c = clothes.objects.filter(category = command[1])
+                    reply_content = u'本季衣服有：\n'
+                    for c in all_clothes:
+                        reply_content += c.name + '\n'
+                    return render_to_response('wx_reply_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content})
 
             pattern_rename = r'^rename'
             p = re.compile(pattern_rename)
@@ -146,7 +152,7 @@ def home(req):
                 return render_to_response('wx_reply_image_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content, 'picUrl': picUrl})
 
             # nothing match, display menu        
-            reply_content = u'帮助菜单\nrename 给衣服起个名字\nshow 显示衣服\nshowall 列出全部衣服\nset cat 设置类型\n  1 - business\n  2 - business casual\n  3 - casual\n  4 - sport\nset sea 设置季节\n  1 - spring and autumn\n  2 - summer\n  3 - winter\ntoday cat sea 今天穿什么\ndel 删除\nchoose 今天选这件'
+            reply_content = u'帮助菜单\nrename 给衣服起个名字\nshow 显示衣服\nshowall 列出全部衣服\nshowcat 按类型查找\nshowsea 按季节查找\nset cat 设置类型\n  1 - business\n  2 - business casual\n  3 - casual\n  4 - sport\nset sea 设置季节\n  1 - spring and autumn\n  2 - summer\n  3 - winter\ntoday cat sea 今天穿什么\ndel 删除\nchoose 今天选这件'
             return render_to_response('wx_reply_text.xml', {'fromUser': toUser, 'toUser': fromUser, 'createTime': int(time.time()), 'content': reply_content})
 
         if msgType == 'image':
