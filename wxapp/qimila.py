8cab273daab68d4629dcbae6f1449d5ced308172
nickyzheng <nicky.zheng@avnet.com>
2015-05-04 15:43:12 +0800

    qimila

--- wxapp/qimila.py
+++ wxapp/qimila.py
@@ -15,7 +15,7 @@ def get_show():
 		print '[-]', 'status_code =', r.status_code
 		os.exit()
 
-	soup = BeautifulSoup(r.text.encode('utf-8'))
+	soup = BeautifulSoup(r.text)
 
 	# for item in soup.find_all('a', title=re.compile(u'少康战情')):
 	# 	print item['title']
@@ -28,7 +28,7 @@ def get_show():
 		item1_url = main_url + item1['href']
 		print '[+]', item1_url
 		r = requests.get(item1_url)
-		soup = BeautifulSoup(r.text.encode('utf-8'))
+		soup = BeautifulSoup(r.text)
 		item1_baidu = soup.find_all('a', href=re.compile(r'pan\.baidu'))[0]['href']
 		print '[+]', 'item1_baidu', item1_baidu
 
@@ -36,7 +36,7 @@ def get_show():
 		item2_url = main_url + item2['href']
 		print '[+]', item2_url
 		r = requests.get(item2_url)
-		soup = BeautifulSoup(r.text.encode('utf-8'))
+		soup = BeautifulSoup(r.text)
 		item2_baidu = soup.find_all('a', href=re.compile(r'pan\.baidu'))[0]['href']
 		print '[+]', 'item2_baidu', item2_baidu
 
